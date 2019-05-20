91b9adc72f3468f0d12e7f3c139aec45d1407922
masiacra <masiacra@gmail.com>
2019-05-20 10:06:47 +0300

    Added methods for tokens obj: get, put; added method for helpers - createRandomString

--- index.js
+++ index.js
@@ -99,6 +99,7 @@ function unifiedServer(req, res) {
 //Define a request router
 const router = {
 	'ping': handlers.ping,
-	'users': handlers.users
+	'users': handlers.users,
+	'tokens': handlers.tokens
 };
 
